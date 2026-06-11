f4256e0c5012db734a4ebcb5c702e6a1aa0faca2
heihei-450 <zjr020401@163.com>
2026-06-11 19:01:04 +0800

    Path Instructions

--- train.py
+++ train.py
@@ -2,14 +2,13 @@ from ultralytics import YOLO
 import cv2 as cv
 
 if __name__ == '__main__':
-    model = YOLO('/home/liu/zjr/zjr_ultralytics_two_stream/model/yolov8m-seg-2stream-add-postfusion-zjr.yaml')  # build a new model from YAML
-    # 预训练模型  c
-    #pre_model_path = r'/home/liu/zjr/zjr_ultralytics_two_stream/weights/权重以及配置文件/权重/yolov11m-2stream-seg/weights/best.pt'
+    model = YOLO('Yaml Path')  # build a new model from YAML
+
     """
     更多详细配置在   ultralytics/yolo/cfg/default.yaml
     """
-    model.train(data="/home/liu/zjr/zjr_ultralytics_two_stream/dataset/LLVIP.yaml", epochs=100, imgsz=640, device=0, batch=4, amp=False,
-                project='/home/liu/zjr/zjr_ultralytics_two_stream/meruns',
+    model.train(data="datasets Path", epochs=100, imgsz=640, device=0, batch=4, amp=False,
+                project='run project path',
                 name='yolo11-seg',
                 # 是否覆盖原来的目录
                 exist_ok=True,
@@ -18,11 +17,10 @@ if __name__ == '__main__':
                 deterministic=False)
 
     # 推理
-    # model = YOLO(r'D:\project\Python\ultralytics_two_stream\weights\add_jet_0.901.pt')  # load a custom model
+    # model = YOLO(r'weight path .pt')  # load a custom model
     #
     # # Predict with the model
-    # results = model([r'D:\project\Python\ultralytics_two_stream\catch_result\论文\color.png',r'D:\project\Python\ultralytics_two_stream\catch_result\论文\depth.png'])  # predict on an image*
+    # results = model([r'rgb path',r'depth path'])  # predict on an image*
     # # 保存结果图
     # res_plotted = results[0].plot(labels=False)
-    # # 保存至photo文件夹下
     # cv.imwrite("reslt.png", res_plotted)
